c6741275eeb827514796fd75da5a3292adf9eac9
khoakomlem <khoakomlem@gmail.com>
2023-02-24 01:11:11 +0700

    + CoolDownSystem

---
+++ src/util/CoolDownSystem.ts
@@ -0,0 +1,27 @@
+import {type ITickData} from '../types.js';
+
+export default class CoolDownSystem {
+	private readonly list = new Map<string, number>();
+
+	update(tickData: ITickData) {
+		this.list.forEach((value, key) => {
+			this.list.set(key, value - tickData.deltaMs);
+		});
+	}
+
+	isCoolingDown(key = 'default') {
+		return this.list.get(key)! > 0;
+	}
+
+	add(key: string, time: number) {
+		this.list.set(key, time);
+	}
+
+	remove(key: string) {
+		this.list.delete(key);
+	}
+
+	clear() {
+		this.list.clear();
+	}
+}
