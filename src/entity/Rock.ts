82b2bd11f7a7cc1d356bb6fcf0660d1e0e1a5950
khoakomlem <khoakomlem@gmail.com>
2023-03-28 23:10:16 +0700

    add health for Rock

--- src/entity/Rock.ts
+++ src/entity/Rock.ts
@@ -1,6 +1,9 @@
 import {type Response, Circle, SATVector, type Body} from 'detect-collisions';
 import Entity from './Entity.js';
 import {type stats, getStats} from '../stats.js';
+import type Bullet from './Bullet.js';
+import {type ITickData} from '../types.js';
+import type World from '../world/World.js';
 
 export default class Rock extends Entity {
 	stats = getStats('Rock');
@@ -10,6 +13,19 @@ export default class Rock extends Entity {
 	constructor(pos = new SATVector(0, 0)) {
 		super();
 		this.body = new Circle(pos, this.stats.radius);
+	}
+
+	update(world: World, tickData: ITickData): void {
+	}
+
+	onCollisionEnter(other: Entity, response: Response): void {
+		if (other.constructor.name === 'Bullet') {
+			this.stats.health -= (other as Bullet).speed / 5;
+			this.body.setScale(this.stats.health / 100);
+			if (this.stats.health <= 30) {
+				this.destroy();
+			}
+		}
 	}
 
 	onCollisionStay(other: Entity, response: Response) {
