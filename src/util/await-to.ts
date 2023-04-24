1e0ec2641817bc163ebd252d861ad0c612ba63b5
khoakomlem <khoakomlem@gmail.com>
2023-04-24 22:54:52 +0700

    utils: await to

---
+++ src/util/await-to.ts
@@ -0,0 +1,15 @@
+export async function to<T, U = Error>(promise: Promise<T>, errorExt?: Record<string, unknown>): Promise<[U, undefined] | [undefined, T]> {
+	try {
+		const data = await promise;
+		return [undefined, data];
+	} catch (err) {
+		if (errorExt) {
+			return [
+				{...(err as U), ...errorExt},
+				undefined,
+			];
+		}
+
+		return [err as U, undefined];
+	}
+}
