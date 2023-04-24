159b7da4f7bcffedaec29ca5ab1600bfd0eb992b
khoakomlem <khoakomlem@gmail.com>
2023-04-24 22:53:14 +0700

    add more tolerance depend on moving speed

--- src/item/Ak47.ts
+++ src/item/Ak47.ts
@@ -6,8 +6,6 @@ import {type ITickData} from '../types.js';
 import type World from '../world/World.js';
 import Gun from './Gun.js';
 
-const tolerance = random.normal(0, Math.PI / 128);
-
 export default class Ak47 extends Gun {
 	constructor() {
 		super(120);
@@ -17,6 +15,7 @@ export default class Ak47 extends Gun {
 		await player.event.emit('shoot');
 		if (!player.isOnline) {
 			// Only create bullet if playing locally (offline)
+			const tolerance = random.normal(0, Math.PI / (128 / (0.5 + player.getSpeedV().len())));
 			const bullet = new Bullet(new SATVector(
 				player.entity.body.pos.x + Math.cos(player.entity.body.angle) * 60,
 				player.entity.body.pos.y + Math.sin(player.entity.body.angle) * 60,
